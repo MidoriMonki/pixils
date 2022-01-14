2e642811e98c5630e843c7f791c81a631760e37f
Glitch (glitch-hello-website) <none>
2022-01-14 11:06:53 +0000

    🖇🚏 Checkpoint ./style.css:18663160/159 ./index.html:18663160/1120 ./script.js:18663160/1549

---
+++ script.js
@@ -0,0 +1,26 @@
+var storeAnswer = [0, 0, 0, 0,
+                   0, 1, 1, 0,
+                   0, 1, 1, 0,
+                   0, 0, 0, 0]
+
+var storePuzzle = [0, 0, 0, 0,
+                   0, 0, 0, 0,
+                   0, 0, 0, 0,
+                   0, 0, 0, 0]
+
+
+
+
+// Add event listener on keydown
+document.addEventListener('keydown', (event) => {
+  
+  if (event.code == "ArrowRight")
+  {
+      
+  }
+    if (event.code == "ArrowRight")
+  {
+      
+  }
+    
+});
