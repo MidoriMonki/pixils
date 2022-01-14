91f0e5abfba350db9712af337155c8bddaf35b17
Glitch (glitch-hello-website) <none>
2022-01-14 11:41:54 +0000

    🎄👆 Checkpoint ./script.js:18663160/815 ./style.css:18663160/142 ./index.html:18663160/550

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ var storePuzzle = [0, 0, 0, 0,
                    0, 0, 0, 0]
 
 
+var storePositions = [0, 1, 2, 3, 0,
+                      0, 4, 5, 6, 0,
+                      0, 7, 8, 9, 0]
 
 
+
+var move = 0;
 // Add event listener on keydown
 document.addEventListener('keydown', (event) => {
   
@@ -20,7 +25,16 @@ document.addEventListener('keydown', (event) => {
   }
   if (event.code == "Digit1")
   {
-      alert("yo");
+    move = 1;
+    for (var i = 0; i < 5; i++)
+    {
+        if (storePositions[i] != 0)
+        {
+            document.getElementById(i).classList.add("test");
+        }
+    }
   }
     
 });
+
+document.getElementById("nah").appendChild(document.getElementById("ya").childNodes[0]);
