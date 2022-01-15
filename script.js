9a00f8664a3cc29fca082756606061c54d3511b8
Glitch (glitch-hello-website) <none>
2022-01-15 02:53:22 +0000

    🚉😺 Checkpoint ./script.js:18663160/4061

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ document.addEventListener('keydown', (event) => {
   
   if (event.code == "Digit1")
   {
-    moveH = "1";
+    moveH = "2";
     moveV = "5";
     Horizontal();
     for (var i = 0; i < 4; i++)
@@ -53,25 +53,26 @@ document.addEventListener('keydown', (event) => {
 function Horizontal()
 {
   if (document.getElementById("slide" + moveH).children.length == 0) {
+    
+  document.getElementById("slide4").appendChild(document.getElementById("slide5").childNodes[3]);
+  document.getElementById("slide4").appendChild(document.getElementById("slide6").childNodes[3]);
+  document.getElementById("slide4").appendChild(document.getElementById("slide7").childNodes[3]);
+  document.getElementById("slide4").appendChild(document.getElementById("slide8").childNodes[3]);
+    
+  document.getElementById("slide3").appendChild(document.getElementById("slide5").childNodes[2]);
+  document.getElementById("slide3").appendChild(document.getElementById("slide6").childNodes[2]);
+  document.getElementById("slide3").appendChild(document.getElementById("slide7").childNodes[2]);
+  document.getElementById("slide3").appendChild(document.getElementById("slide8").childNodes[2]);   
+        
+  document.getElementById("slide2").appendChild(document.getElementById("slide5").childNodes[1]);
+  document.getElementById("slide2").appendChild(document.getElementById("slide6").childNodes[1]);
+  document.getElementById("slide2").appendChild(document.getElementById("slide7").childNodes[1]);
+  document.getElementById("slide2").appendChild(document.getElementById("slide8").childNodes[1]);
+    
   document.getElementById("slide1").appendChild(document.getElementById("slide5").childNodes[0]);
   document.getElementById("slide1").appendChild(document.getElementById("slide6").childNodes[0]);
   document.getElementById("slide1").appendChild(document.getElementById("slide7").childNodes[0]);
-  document.getElementById("slide1").appendChild(document.getElementById("slide8").childNodes[0]);
-    
-  document.getElementById("slide2").appendChild(document.getElementById("slide5").childNodes[0]);
-  document.getElementById("slide2").appendChild(document.getElementById("slide6").childNodes[0]);
-  document.getElementById("slide2").appendChild(document.getElementById("slide7").childNodes[0]);
-  document.getElementById("slide2").appendChild(document.getElementById("slide8").childNodes[0]);
-    
-  document.getElementById("slide3").appendChild(document.getElementById("slide5").childNodes[0]);
-  document.getElementById("slide3").appendChild(document.getElementById("slide6").childNodes[0]);
-  document.getElementById("slide3").appendChild(document.getElementById("slide7").childNodes[0]);
-  document.getElementById("slide3").appendChild(document.getElementById("slide8").childNodes[0]);
-    
-  document.getElementById("slide4").appendChild(document.getElementById("slide5").childNodes[0]);
-  document.getElementById("slide4").appendChild(document.getElementById("slide6").childNodes[0]);
-  document.getElementById("slide4").appendChild(document.getElementById("slide7").childNodes[0]);
-  document.getElementById("slide4").appendChild(document.getElementById("slide8").childNodes[0]);
+  document.getElementById("slide1").appendChild(document.getElementById("slide8").childNodes[0]);   
   }
 }
 
