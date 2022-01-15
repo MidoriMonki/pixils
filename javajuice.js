bf6d9e4ae7c08dbcb55bd8077282dbe2daf01b8f
Glitch (glitch-hello-website) <none>
2022-01-15 08:12:34 +0000

    👧💘 Checkpoint ./javajuice.js:18663160/532

--- javajuice.js
+++ javajuice.js
@@ -150,15 +150,16 @@ function checkDirection()
       mode = 0;
       for (var i=0;i<25;i++){
          if (storePositions[i] == target){
-             if (storePositions[i][1] == 0 || storePositions[i][1] == 5){
+             var cheese = i.toString()[i.toString().length - 1];
+             if (i[1] == 0 || i[1] == 5){
              row = 1;   
-             }else if (storePositions[i][1] == 1 || storePositions[i][1] == 6){
+             }else if (cheese == 1 || cheese == 6){
              row = 2;
-             }else if (storePositions[i][1] == 2 || i[i.length - 1] == 7){
+             }else if (cheese == 2 || cheese == 7){
              row = 3;  
-             }else if (i[i.length - 1] == 3 || i[i.length - 1] == 8){
+             }else if (cheese == 3 || cheese == 8){
              row = 4;     
-             }else if (i[i.length - 1] == 4 || i[i.length - 1] == 9){
+             }else if (cheese == 4 || cheese == 9){
              row = 5;
            }    
          }
