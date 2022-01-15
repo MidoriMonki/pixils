b81dafa3e8103bf4cb095bb2acc83af4d28034a1
Glitch (glitch-hello-website) <none>
2022-01-15 17:02:43 +0000

    🍱👧 Checkpoint ./brokenParty.js:18663160/1162

--- brokenParty.js
+++ brokenParty.js
@@ -1,10 +1,10 @@
-var row = 1;
+var row = 0;
 var vPosX;
 var uPosX;
 var vPosY;
 var uPosY;
 var target;
-var mode = 1;
+var mode = 2;
 var pixilsInRow;
 var slide = document.getElementById("slide1");
 var storePositions = [0, 0, 0, 0, 0,
@@ -20,17 +20,11 @@ var storePositions = [0, 0, 0, 0, 0,
 
 for (var i=0;i<9;i++)
 {
-  if (slide.children[i].className != "null")
-    {  
     slide.children[i].addEventListener('touchstart', dragStart);
     slide.children[i].addEventListener('touchend', dragEnd);
     slide.children[i].addEventListener('touchmove', dragMove);
     slide.children[i].mousedown = dragStart;
-    }
 }
-
-
-
 
 
 //Mobile Start
@@ -77,13 +71,6 @@ function dragMove(e)
   if (mode == 2){
     checkDirection();
   }
-  
-  // let us begin scrolling
-  if(mode == 1){
-    //for (var i=0;i<5;i++){slide.children[pixilsInRow[i] - 1].style.right = uRight[row-1] - (vPosX - uPosX) + 15*(i-2) + "vw";}
-  } else if (mode == 0){
-    //for (var i=0;i<5;i++){slide.children[pixilsInRow[i] - 1].style.top = uTop[row-1] - (uPosY - vPosY) + 15*(i-2) + "vw";}
-  }
 }
 
 
@@ -94,15 +81,15 @@ function dragMove(e)
 function dragEnd()
 {
   if (mode == 1){
-      if (vPosX < uPosX){
+      if (vPosX < uPosX && storePositions[row*5] == 0){
         //left
-        for(var i=(row*5);i<(5+(row*5));i++){
+        for(var i=(row*5);i<(4+(row*5));i++){
           storePositions[i] = storePositions[i+1];
         }
         storePositions[(row*5)+4] = 0;
-      }else{
+      }else if(vPosX > uPosX && storePositions[(row*5)+4] == 0){
         //right
-        for(var i=(row*5)+5;i>(row*5);i--){
+        for(var i=(row*5)+4;i>(row*5);i--){
           storePositions[i] = storePositions[i-1];
         }
         storePositions[(row*5)] = 0;
@@ -110,7 +97,7 @@ function dragEnd()
       //Set their right property correctly
       for(var i=(row*5);i<(5+(row*5));i++){
         if (storePositions[i] != 0){
-            slide.children[storePositions[i]].style.right = (i-(row*5))*15 + "vw";
+            slide.children[storePositions[i]-1].style.right = (2-(i-(row*5)))*15 + "vw";
         }
       } 
   
@@ -122,7 +109,7 @@ function dragEnd()
       }
   }
   
-  
+  mode = 2;
   document.getElementById("bruh").innerHTML = storePositions;
 }
 
@@ -138,62 +125,37 @@ function checkDirection()
        for (var i=0;i<25;i++){
          if (storePositions[i] == target){
              if (i <= 4){
-             row = 1;   
+             row = 0;   
              }else if (i <= 9){
-             row = 2;
+             row = 1;
              }else if (i <= 14){
-             row = 3;  
+             row = 2;  
              }else if (i <= 19){
-             row = 4;     
+             row = 3;     
              }else if (i <= 24){
-             row = 5;      
-           }    
+             row = 4;      
+           } 
          }
        }
-         for (var i=0;i<5;i++){
-           pixilsInRow[i] = storePositions[((row-1)*5) + i];
-         }
   }else{
       mode = 0;
       for (var i=0;i<25;i++){
          if (storePositions[i] == target){
              var cheese = i.toString()[i.toString().length - 1];
              if (cheese == 0 || cheese == 5){
-             row = 1;   
+             row = 0;   
              }else if (cheese == 1 || cheese == 6){
-             row = 2;
+             row = 1;
              }else if (cheese == 2 || cheese == 7){
-             row = 3;  
+             row = 2;  
              }else if (cheese == 3 || cheese == 8){
-             row = 4;     
+             row = 3;     
              }else if (cheese == 4 || cheese == 9){
-             row = 5;
+             row = 4;
            }    
          }
        }
-         for (var i=0;i<0;i++){
-            pixilsInRow[i] = storePositions[(row-1) + (5*i)];
-         }
-         document.getElementById("bruhY").innerHTML = pixilsInRow;
     }
 }
 
-
-
-
-
-
-
-
-
-//right
-for(var i=(row*5)+5;i>(row*5);i--){
-  storePositions[i] = storePositions[i-1];
-}
-storePositions[(row*5)] = 0;
-//left
-for(var i=(row*5);i<(5+(row*5));i++){
-  storePositions[i] = storePositions[i+1];
-}
-storePositions[(row*5)+4] = 0;
 document.getElementById("bruh").innerHTML = storePositions;
