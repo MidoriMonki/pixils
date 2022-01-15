66137feea38d2e0c7906260032ea6c2c08b226ac
Glitch (glitch-hello-website) <none>
2022-01-15 03:40:31 +0000

    🍡🍂 Checkpoint ./index.html:18663160/1848 ./style.css:18663160/47 ./javajuice.js:18663160/6539

---
+++ javajuice.js
@@ -0,0 +1,17 @@
+addEventListener('touchstart', dragStart)
+addEventListener('touchend', dragEnd)
+addEventListener('touchmove', dragMove)
+
+
+function dragStart()
+{
+  alert("yah");
+}
+function dragEnd()
+{
+  
+}
+function dragMove()
+{
+  
+}
